5366754073f6fda10afe27204f0c7a98a9e4331f
Luca Tomarelli <lucatomarelli1@gmail.com>
2026-05-15 14:53:13 +0200

    dockerize db with pgvector image, add embedding column

--- prisma/migrations/20260515124328/migration.sql
+++ prisma/migrations/20260515124328/migration.sql
@@ -83,6 +83,9 @@ CREATE TABLE "Message" (
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "Workspace_userId_name_key" ON "Workspace"("userId", "name");
+
 -- AddForeignKey
 ALTER TABLE "Workspace" ADD CONSTRAINT "Workspace_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
